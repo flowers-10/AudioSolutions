8aa7d27221a4e46c474809db8b8b3171fac50639
Michela <mgaligigi@163.com>
2024-05-12 21:35:33 +0800

    chore:add core library dependence

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
+import glsl from "vite-plugin-glsl"; // vite扩展glsl语言
+import tailwindcss from 'tailwindcss' // 导入tailwindcss
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "../src"),
+      "@assets" : path.resolve(__dirname, "../src/assets")
+    },
+  },
+  plugins: [
+    vue(),
+    glsl({
+      compress: true,
+      watch: true,
+    }),
+  ],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+      ],
+    },
+  },
 });
