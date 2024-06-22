97bef4500c148a868fedb99cee28e0d8eb784380
Michela <mgaligigi@163.com>
2024-06-22 23:00:19 +0800

    feat:three audio examples

--- src/components/AsAudio/index.vue
+++ src/components/AsAudio/index.vue
@@ -13,37 +13,108 @@
 
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
-import * as THREE from 'three';
+import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
-const mediaElement = ref()
-// const play = () => {
-//   mediaElement.value.play();
-// };
-// const pause = () => {
-//   mediaElement.value.pause();
-// };
-const stop = () => {
-  // mediaElement.value.stop();
-};
-onMounted(async () => {
-  const fftSize = 128;
-  const listener = new THREE.AudioListener();
-  const audio = new THREE.Audio(listener);
-  const file = '../../static/audio/Avicii-WeBurn.mp3';
-  mediaElement.value = new Audio(file);
-  const analyser = new THREE.AudioAnalyser(audio, fftSize);
-  mediaElement.value.play();
-  const uniforms = {
-    tAudioData: { value: new THREE.DataTexture(analyser.data, fftSize / 2, 1, THREE.RedFormat) }
-  };
+const mediaElement = ref();
+const analyser = ref();
+const uniform = ref();
+let scene:any
+let camera:any
+let renderer:any
+const fftSize = 128;
+const clock = new THREE.Clock();
 
+const init = () => {
   const canvas = document.querySelector("canvas.webgl");
   // Scene
-  const scene = new THREE.Scene();
+  scene = new THREE.Scene();
+
+  const material = new THREE.MeshBasicMaterial()
+  const geometry = new THREE.PlaneGeometry(1, 1);
+  const mesh = new THREE.Mesh(geometry, material);
+  // scene.add(mesh);
+  /**
+   * Sizes
+   */
+  const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  };
+  window.addEventListener("resize", () => {
+    // Update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+    // Update camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  });
+
+  /**
+   * Renderer
+   */
+  renderer = new THREE.WebGLRenderer({
+    canvas: canvas as HTMLElement,
+  });
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  renderer.setClearColor("#111");
+  /**
+   * Camera
+   */
+  // Base camera
+  camera = new THREE.PerspectiveCamera(
+    75,
+    sizes.width / sizes.height,
+    0.1,
+    100
+  );
+  camera.position.set(0, 0, 1);
+  scene.add(camera);
+  // Controls
+
+  // controls.enableDamping = true;
+};
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+  // Update material
+  analyser.value?.getFrequencyData && analyser.value.getFrequencyData();
+  console.log(analyser.value?.data);
+  if (uniform.value?.tAudioData) {
+    uniform.value.tAudioData.value.needsUpdate = true;
+  }
+
+  // Render
+  renderer.render(scene, camera);
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+const play = () => {
+  const listener = new THREE.AudioListener();
+  const audio = new THREE.Audio(listener);
+  const file = "../../static/audio/Avicii-WeBurn.mp3";
+  mediaElement.value = new Audio(file);
+  mediaElement.value.play();
+  audio.setMediaElementSource(mediaElement.value);
+  analyser.value = new THREE.AudioAnalyser(audio, fftSize);
+  uniform.value = {
+    tAudioData: {
+      value: new THREE.DataTexture(
+        analyser.value.data,
+        fftSize / 2,
+        1,
+        THREE.RedFormat
+      ),
+    },
+  };
 
   const material = new THREE.ShaderMaterial({
-    uniforms: uniforms,
+    uniforms: uniform.value,
     vertexShader: `
 			varying vec2 vUv;
 
@@ -69,73 +140,13 @@ onMounted(async () => {
 				gl_FragColor = vec4( mix( backgroundColor, color, i ), 1.0 );
 
 			}`,
-
   });
   const geometry = new THREE.PlaneGeometry(1, 1);
   const mesh = new THREE.Mesh(geometry, material);
   scene.add(mesh);
-
-  /**
-      * Sizes
-      */
-  const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight,
-  };
-  window.addEventListener("resize", () => {
-    // Update sizes
-    sizes.width = window.innerWidth;
-    sizes.height = window.innerHeight;
-    // Update camera
-    camera.aspect = sizes.width / sizes.height;
-    camera.updateProjectionMatrix();
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height);
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-  });
-
-  /**
-       * Renderer
-       */
-  const renderer = new THREE.WebGLRenderer({
-    canvas: canvas as HTMLElement,
-  });
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-  renderer.setClearColor("#111");
-  /**
-   * Camera
-   */
-  // Base camera
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    sizes.width / sizes.height,
-    0.1,
-    100
-  );
-  camera.position.set(0, 0, 1);
-  scene.add(camera);
-  // Controls
-  const controls = new OrbitControls(camera, canvas as HTMLElement);
-  // controls.enableDamping = true;
-  /**
-     * Animate
-     */
-  const clock = new THREE.Clock();
-
-  const tick = () => {
-    const elapsedTime = clock.getElapsedTime();
-    // Update controls
-    controls.update();
-    // Update material
-    uniforms.tAudioData.value.needsUpdate = true;
-
-    // Render
-    renderer.render(scene, camera);
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick);
-  };
-
+};
+onMounted(async () => {
+  init();
   tick();
 });
 </script>
