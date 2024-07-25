ffd36d83884a9a7a89b6d13793ab0e6e49506c4d
Michela <mgaligigi@163.com>
2024-07-25 15:52:47 +0800

    fix:vercel load

--- src/components/AsArtful/index.vue
+++ src/components/AsArtful/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
       <audio ref="audio" crossorigin="anonymous">
-        <source src="../../../static/audio/Avicii-WeBurn.mp3" type="audio/mpeg" />
+        <source src="/audio/Avicii-WeBurn.mp3" type="audio/mpeg" />
       </audio>
       <button style="z-index: 100; position: absolute" @click="sSource.start()">
         Play
