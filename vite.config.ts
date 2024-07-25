3916502a9a1ad011020238dedfbed9387e607191
Michela <mgaligigi@163.com>
2024-07-25 14:38:17 +0800

    chore:vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,24 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 import glsl from "vite-plugin-glsl"; // vite扩展glsl语言
-import tailwindcss from 'tailwindcss' // 导入tailwindcss
+import tailwindcss from "tailwindcss"; // 导入tailwindcss
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
+  base: "./",
+  publicDir: "./static",
+  assetsInclude: ["**/*.glb", "**/*.hdr", "**/*.mp3", "**/*.ico","**/*.svg"],
+  server: {
     port: 9712,
   },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
       "@static": path.resolve(__dirname, "static"),
-      "@assets" : path.resolve(__dirname, "src/assets"),
-      "@shaders" : path.resolve(__dirname, "src/shaders"),
-      "@utils" : path.resolve(__dirname, "src/utils"),
-      "@components" : path.resolve(__dirname, "src/components"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@shaders": path.resolve(__dirname, "src/shaders"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@components": path.resolve(__dirname, "src/components"),
     },
   },
   plugins: [
@@ -28,9 +31,7 @@ export default defineConfig({
   ],
   css: {
     postcss: {
-      plugins: [
-        tailwindcss,
-      ],
+      plugins: [tailwindcss],
     },
   },
 });
